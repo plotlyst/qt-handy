c3cabe8bf3d9f221b2155f7b5b86331a74364859
Zsolt Kovari <zsolt@kovaridev.com>
2022-02-21 10:46:42 +0100

    Add InstantTooltipEventFilter (#15)
    * Add InstantTooltipEventFilter
    
    * Extra assert
    
    * remove prints

---
+++ qthandy/filter.py
@@ -0,0 +1,16 @@
+from qtpy.QtCore import QObject, QEvent
+from qtpy.QtGui import QCursor
+from qtpy.QtWidgets import QWidget, QToolTip
+
+
+class InstantTooltipEventFilter(QObject):
+    def __init__(self, parent=None):
+        super(InstantTooltipEventFilter, self).__init__(parent)
+
+    def eventFilter(self, watched: QObject, event: QEvent) -> bool:
+        if isinstance(watched, QWidget) and event.type() == QEvent.Enter:
+            QToolTip.showText(QCursor.pos(), watched.toolTip())
+        elif event.type() == QEvent.Leave:
+            QToolTip.hideText()
+
+        return super(InstantTooltipEventFilter, self).eventFilter(watched, event)

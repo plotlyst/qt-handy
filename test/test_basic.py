1c8d099837455fd052ad18096b3e413dba45a150
zkovari <zsolt@kovaridev.com>
2022-09-30 11:09:16 +0200

    Add more tests

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,6 +1,6 @@
-from qtpy.QtWidgets import QLabel
+from qtpy.QtWidgets import QLabel, QWidget, QPushButton, QApplication, QToolButton, QMessageBox
 
-from qthandy import translucent
+from qthandy import translucent, hbox, retain_when_hidden, spacer, transparent, busy, btn_popup, ask_confirmation
 
 
 def test_translucent(qtbot):
@@ -11,3 +11,92 @@ def test_translucent(qtbot):
     translucent(widget)
 
     assert widget.graphicsEffect()
+
+
+def test_retain_when_hidden(qtbot):
+    parent = QWidget()
+    parent.setFixedWidth(300)
+    hbox(parent)
+    stretched_btn = QPushButton('Stretched')
+    btn = QPushButton()
+    btn.setFixedWidth(100)
+    parent.layout().addWidget(stretched_btn)
+    parent.layout().addWidget(btn)
+
+    qtbot.addWidget(parent)
+    parent.show()
+
+    prev_btn_size = stretched_btn.width()
+    retain_when_hidden(btn)
+    btn.setHidden(True)
+    qtbot.wait(5)
+    assert prev_btn_size == stretched_btn.width()
+
+
+def test_spacer(qtbot):
+    parent = QWidget()
+    parent.setFixedWidth(300)
+    hbox(parent)
+
+    btn = QPushButton('Button')
+    btn.setMinimumWidth(100)
+
+    parent.layout().addWidget(spacer())
+    parent.layout().addWidget(btn)
+    qtbot.addWidget(parent)
+    parent.show()
+
+    assert btn.width() == 100
+
+
+def test_spacer_with_max_stretch(qtbot):
+    parent = QWidget()
+    parent.setFixedWidth(300)
+    hbox(parent, 0, 0)
+
+    btn = QPushButton('Button')
+    btn.setMinimumWidth(100)
+
+    parent.layout().addWidget(spacer(max_stretch=150))
+    parent.layout().addWidget(btn)
+    qtbot.addWidget(parent)
+    parent.show()
+
+    assert btn.width() == 150
+
+
+def test_transparent_label(qtbot):
+    lbl = QLabel('Test')
+    transparent(lbl)
+
+
+def test_busy(qtbot):
+    @busy
+    def busy_func():
+        assert QApplication.overrideCursor()
+
+    assert not QApplication.overrideCursor()
+    busy_func()
+    assert not QApplication.overrideCursor()
+
+
+def test_btn_popup(qtbot):
+    btn = QToolButton()
+    qtbot.addWidget(btn)
+    btn.show()
+
+    lbl = QLabel('Test')
+    btn_popup(btn, lbl)
+
+    assert btn.menu()
+    assert btn.popupMode() == QToolButton.ToolButtonPopupMode.InstantPopup
+
+
+def test_confirmation(qtbot, monkeypatch):
+    monkeypatch.setattr(QMessageBox, "question", lambda *args: QMessageBox.Yes)  # confirm
+    confirmed = ask_confirmation('Confirmation')
+    assert confirmed
+
+    monkeypatch.setattr(QMessageBox, "question", lambda *args: QMessageBox.No)  # confirm
+    confirmed = ask_confirmation('Confirmation')
+    assert not confirmed

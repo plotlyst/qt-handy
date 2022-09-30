d10389193b3dd70d092f9cb0eb0004dcd8c963d2
zkovari <zsolt@kovaridev.com>
2022-09-30 14:30:31 +0200

    Add DragEventFilter

--- test/test_filter.py
+++ test/test_filter.py
@@ -1,9 +1,11 @@
 import platform
+from unittest.mock import create_autospec
 
 import pytest
-from qtpy.QtWidgets import QPushButton
+from qtpy.QtCore import Qt, QEvent, QTimer
+from qtpy.QtWidgets import QPushButton, QLabel
 
-from qthandy.filter import InstantTooltipEventFilter
+from qthandy.filter import InstantTooltipEventFilter, DragEventFilter
 
 
 @pytest.mark.skipif(platform.system() == 'Darwin', reason="Cannot run on Darwin")
@@ -16,3 +18,30 @@ def test_instant_tooltip(qtbot):
     btn.installEventFilter(InstantTooltipEventFilter(btn))
     qtbot.wait(50)
     qtbot.mouseMove(btn)
+
+
+def drop(qtbot, wdg):
+    qtbot.mouseRelease(wdg, Qt.LeftButton, delay=30)
+
+
+def test_drag(qtbot):
+    label = QLabel('Test label')
+    filter = DragEventFilter(label, 'application/text', lambda x: 'data')
+    label.installEventFilter(filter)
+    qtbot.addWidget(label)
+    label.show()
+
+    qtbot.wait(50)
+
+    with qtbot.waitSignals([filter.dragStarted, filter.dragFinished]):
+        qtbot.mouseMove(label)
+        qtbot.mousePress(label, Qt.LeftButton, delay=30)
+
+        event = create_autospec(QEvent)
+        event.type = lambda: QEvent.MouseMove
+        event.pos = lambda: label.rect().center()
+        QTimer.singleShot(100, lambda: drop(qtbot, label))
+        try:
+            filter.eventFilter(label, event)
+        except TypeError:  # coming from super
+            pass
